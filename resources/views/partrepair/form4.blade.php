b7349871d92af216766fe1d911f2a72ca0bc68f2
rikofebriyan <rikofebriyan@gmail.com>
2022-12-20 10:53:30 +0700

    input standard

--- resources/views/partrepair/form4.blade.php
+++ resources/views/partrepair/form4.blade.php
@@ -1,7 +1,8 @@
 <div class="container-fluid justify-content-center py-0">
-    <div class="mb-3">
-        <button class="btn btn-primary">Add item Pengecekan</button>
-    </div>
+    <button type="button" class="btn btn-md btn-success mb-3 float-right" data-bs-toggle="modal"
+        data-bs-target="#exampleModal">
+        Add Standard Pengecekan
+    </button>
     <div class="table-responsive-sm">
         {{ Form::open(['route' => 'partrepair.progresstrial.store', 'method' => 'POST']) }}
         <table id="myTable" class="table table-striped nowrap overflow-auto display">
@@ -49,3 +50,58 @@
         {{ Form::close() }}
     </div>
 </div>
+
+
+<!-- Modal -->
+
+{{ Form::open(['route' => 'matrix.standard_pengecekan.store', 'method' => 'POST']) }}
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Section</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                {{-- FORM COLUMN 1 --}}
+                <div class="form-group mt-2">
+                    <label for="master_spare_part_id">Section ID</label>
+                    <select name="master_spare_part_id" id="master_spare_part_id" class="form-control">
+                        <option value="{{ $asu->item_id }}">{{ $asu->item_code }}
+                        </option>
+                    </select>
+                </div>
+
+                {{-- FORM COLUMN 1 --}}
+                <div class="form-group mt-2">
+                    <label for="item_standard_id">Section ID</label>
+                    <select name="item_standard_id" id="item_standard_id" class="form-control">
+                        <option value="" disabled selected>
+                            choose
+                        </option>
+                        @foreach ($itemstandard as $tabw)
+                            <option value="{{ $tabw->id }}">
+                                {{ $tabw->item_standard }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                {{-- FORM COLUMN 1 --}}
+                <div class="form-group mt-2">
+                    <label for="standard_pengecekan">standard_pengecekan</label>
+                    <input type="text" id="standard_pengecekan" name="standard_pengecekan" class="form-control"
+                        value="" required>
+                </div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{ Form::close() }}
