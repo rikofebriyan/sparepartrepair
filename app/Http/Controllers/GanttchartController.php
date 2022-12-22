ad57fe51274721a4157299b44ee0ae7f51ec0dd2
rikofebriyan <rikofebriyan@gmail.com>
2022-12-22 14:41:07 +0700

    gantt chart update

--- app/Http/Controllers/GanttchartController.php
+++ app/Http/Controllers/GanttchartController.php
@@ -25,84 +25,114 @@ class GanttchartController extends Controller
      */
     public function index(Request $request)
     {
-        $waitingrepair = Waitingrepair::all();
-        $tester = DB::table('progressrepairs')
-            ->whereMonth('created_at', '=', Carbon::now()->format('m'))
-            ->whereYear('created_at', '=', Carbon::now()->format('Y'))
-            ->get();
-        $tester2 = DB::table('waitingrepairs')
-        ->whereMonth('created_at', '=', Carbon::now()->format('m'))
-        ->whereYear('created_at', '=', Carbon::now()->format('Y'))
-        ->select('item_name')
+        $join = DB::table('waitingrepairs')
+        ->join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
+        // ->join('progresspemakaians', 'waitingrepairs.id', '=', 'progresspemakaians.form_input_id')
+        ->select('waitingrepairs.*', 'progressrepairs.place_of_repair', 'progressrepairs.analisa','progressrepairs.action', 'progressrepairs.plan_start_repair', 'progressrepairs.plan_finish_repair')
         ->get();
 
-        foreach($tester as $index => $value) {
+        $count = count(collect($join));
+        // dd($join);
+        // $waitingrepair = Waitingrepair::all();
+        // $tester = DB::table('progressrepairs')
+        //     ->whereMonth('created_at', '=', Carbon::now()->format('m'))
+        //     ->whereYear('created_at', '=', Carbon::now()->format('Y'))
+        //     ->get();
+            // dd($waitingrepair);
+
+            // $join = Waitingrepair::join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
+            // ->select('waitingrepair.*', 'progressrepairs.place_of_repair', )
+            // ->where('waitingrepair.progress', !=, 'Finish')
+            // ->get();
+            // dd($join);
+        
+        // $tester2 = DB::table('waitingrepairs')
+        // ->whereMonth('created_at', '=', Carbon::now()->format('m'))
+        // ->whereYear('created_at', '=', Carbon::now()->format('Y'))
+        // ->select('item_name')
+        // ->get();
+
+        foreach($join as $index => $value) {
             // dd($value);
             $data[$index] =[
-                'x' => $value->analisa,
-                'y' => $value->created_at,
-                'z' => $value->updated_at,
-                'o' => '2019-03-05',
-                'p' => '2019-03-07'
+                'id' => $value->id,
+                'created_at' => $value->created_at,
+                'updated_at' => $value->updated_at,
+                'date' => $value->date,
+                'part_from' => $value->part_from,
+                'reg_sp' => $value->reg_sp,
+                'section' => $value->section,
+                'line' => $value->line,
+                'machine' => $value->machine,
+                'item_code' => $value->item_code,
+                'item_name' => $value->item_name,
+                'item_type' => $value->item_type,
+                'maker' => $value->maker,
+                'problem' => $value->problem,
+                'nama_pic' => $value->nama_pic,
+                'price' => $value->price,
+                'status_repair' => $value->status_repair,
+                'progress' => $value->progress,
+                'place_of_repair' => $value->place_of_repair,
+                'analisa' => $value->analisa,
+                'action' => $value->action,
+                'plan_start_repair' => $value->plan_start_repair,
+                'plan_finish_repair' => $value->plan_finish_repair
                         ];
         }
-        // dd($data[0]);
 
 
-        $dateNow = Carbon::now();
-        $dateNowYear = $dateNow->format('Y');
-        $dateNowMonth = $dateNow->format('m');
-        $week = [
-            1 => 7,
-            8 => 14,
-            15 => 21,
-            22 => 28,
-            29 => 31
-        ];
-        $stepProgress = ['Waiting', 'On Progress', 'Seal Kit', 'Trial', 'Finish', 'Scrap'];
-        $asu = [
-            'x : Design', 'On Progress', 'Seal Kit', 'Trial', 'Finish', 'Scrap'
-        ];
+        // $dateNow = Carbon::now();
+        // $dateNowYear = $dateNow->format('Y');
+        // $dateNowMonth = $dateNow->format('m');
+        // $week = [
+        //     1 => 7,
+        //     8 => 14,
+        //     15 => 21,
+        //     22 => 28,
+        //     29 => 31
+        // ];
+        // $stepProgress = ['Waiting', 'On Progress', 'Seal Kit', 'Trial', 'Finish', 'Scrap'];
+        // $asu = [
+        //     'x : Design', 'On Progress', 'Seal Kit', 'Trial', 'Finish', 'Scrap'
+        // ];
 
-        $dataRepairAll = DB::table('waitingrepairs')
-            ->whereMonth('date', '=', Carbon::now()->format('m'))
-            ->whereYear('date', '=', Carbon::now()->format('Y'))
-            ->get();
+        // $dataRepairAll = DB::table('waitingrepairs')
+        //     ->whereMonth('date', '=', Carbon::now()->format('m'))
+        //     ->whereYear('date', '=', Carbon::now()->format('Y'))
+        //     ->get();
 
-        foreach ($stepProgress as $step) {
-            $counting[$step] = 0;
-            $w = 1;
-            foreach ($week as $start => $finish) {
-                $qty[$step]['W' . $w] = DB::table('waitingrepairs')
-                    ->whereMonth('date', '=', $dateNowMonth)
-                    ->whereYear('date', '=', $dateNowYear)
-                    ->where('progress', $step)
-                    ->whereBetween('date', [Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $start)->format('Y-m-d'), Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $finish)->format('Y-m-d')])
-                    ->count();
+        // foreach ($stepProgress as $step) {
+        //     $counting[$step] = 0;
+        //     $w = 1;
+        //     foreach ($week as $start => $finish) {
+        //         $qty[$step]['W' . $w] = DB::table('waitingrepairs')
+        //             ->whereMonth('date', '=', $dateNowMonth)
+        //             ->whereYear('date', '=', $dateNowYear)
+        //             ->where('progress', $step)
+        //             ->whereBetween('date', [Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $start)->format('Y-m-d'), Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $finish)->format('Y-m-d')])
+        //             ->count();
 
-                $qty['total']['W' . $w] = DB::table('waitingrepairs')
-                    ->whereMonth('date', '=', $dateNowMonth)
-                    ->whereYear('date', '=', $dateNowYear)
-                    ->whereBetween('date', [Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $start)->format('Y-m-d'), Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $finish)->format('Y-m-d')])
-                    ->count();
+        //         $qty['total']['W' . $w] = DB::table('waitingrepairs')
+        //             ->whereMonth('date', '=', $dateNowMonth)
+        //             ->whereYear('date', '=', $dateNowYear)
+        //             ->whereBetween('date', [Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $start)->format('Y-m-d'), Carbon::parse($dateNowYear . '-' . $dateNowMonth . '-' . $finish)->format('Y-m-d')])
+        //             ->count();
 
-                $counting[$step] = $counting[$step] + $qty[$step]['W' . $w];
-                $w++;
-            }
-        }
+        //         $counting[$step] = $counting[$step] + $qty[$step]['W' . $w];
+        //         $w++;
+        //     }
+        // }
 
-        $counting['total'] = 0;
-        foreach ($qty['total'] as $index => $item) {
-            $counting['total'] = $counting['total'] + $item;
-        }
+        // $counting['total'] = 0;
+        // foreach ($qty['total'] as $index => $item) {
+        //     $counting['total'] = $counting['total'] + $item;
+        // }
 
         return view('partrepair/ganttchart', [
-            'dataRepairAll' => $dataRepairAll,
-            'qty' => $qty,
             'date' => Carbon::now()->format('Y-m'),
-            'counting' => $counting,
-            'data' => $data,
-            'waitingrepair' => $waitingrepair,
+            'count' => $count,
+            'data' => $data
         ]);
     }
 
--- resources/views/partrepair/ganttchart.blade.php
+++ resources/views/partrepair/ganttchart.blade.php
@@ -5,14 +5,22 @@
         <h3 class="m-2">GANTT CHART SCHEDULE REPAIR</h3>
     </div>
 
-    <div class="row m-1">
-        <div class="card col border">
-            <div id="gantt">
+    <div class="row m-1" style="transition: width 0.5s">
+        <div class="card col border" style="transition: width 0.5s">
+            <div id="gantt" class="m-2" style="transition: width 0.5s">
             </div>
         </div>
 
 
-        <div class="card col-4 border mx-2" id="asu" style="display: none">
+        <div class="card col-4 border mx-2" id="asu" style="display: none; transition:width 0.5s; min-height:1000px">
+            <div class="row">
+                <div class="col-3">
+                    <a href="#" type="button" id="hideshow" class="btn icon icon-left btn-light border mt-2"><i
+                            data-feather="x"></i>
+                        Hide</a>
+                </div>
+            </div>
+            {{-- <input type='button' id='hideshow' value='hide/show'> --}}
             <div class="p-3">
                 <div class="d-flex justify-content-between">
                     <div class="p-0">
@@ -32,26 +40,46 @@
 
                 <div class="row">
                     <label for="disabledInput" class="col-sm-3 col-form-label">No. Ticket</label>
-                    <p class="col-sm-9 align-items-center d-flex mb-0" id="testing"></p>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="reg_sp"></p>
                 </div>
 
                 <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Date Created</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        15 Jan 2022
-                    </div>
+                    <label for="disabledInput" class="col-sm-3 col-form-label">PIC Repair</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="nama_pic"></p>
                 </div>
+
                 <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">PIC repair</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        Riko
-                    </div>
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Item Name</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="item_name"></p>
                 </div>
+
                 <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Part Name</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        Robot Gundam
-                    </div>
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Progress</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="progress"></p>
+                </div>
+
+                <div class="row">
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Repair Place</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="place_of_repair"></p>
+                </div>
+
+                <div class="divider m-0">
+                    <div class="divider-text">Problem</div>
+                </div>
+
+                <div class="row">
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Problem</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="problem"></p>
+                </div>
+
+                <div class="row">
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Analisa</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="analisa"></p>
+                </div>
+
+                <div class="row">
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Action</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="action"></p>
                 </div>
 
 
@@ -59,50 +87,16 @@
                     <div class="divider-text">Cost</div>
                 </div>
 
-
-
                 <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Place of Repair</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        Subcont
-                    </div>
+                    <label for="disabledInput" class="col-sm-3 col-form-label">Cost</label>
+                    <p class="col-sm-9 align-items-center d-flex mb-0" id="created_at"></p>
                 </div>
-                <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Subcont Cost</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        10000
-                    </div>
-                </div>
-                <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Labor Cost</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        15000
-                    </div>
-                </div>
-                <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Seal Kit Cost</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        15000
-                    </div>
-                </div>
-                <div class="row">
-                    <label for="disabledInput" class="col-sm-3 col-form-label">Total Cost</label>
-                    <div class="col-sm-9 align-items-center d-flex">
-                        15000
-                    </div>
-                </div>
-
-
-                <div class="divider m-0">
-                    <div class="divider-text">Trial Result</div>
-                </div>
-                <div class="divider m-0">
-                    <input type='button' id='hideshow' value='hide/show'>
-                </div>
-
 
             </div>
+
+
         </div>
+    </div>
     </div>
 @endsection
 
@@ -197,10 +191,10 @@
                     data: [
                         @foreach ($data as $dt)
                             {
-                                x: '{{ $dt['x'] }}',
+                                x: '{{ $dt['id'] . ' ' . $dt['item_name'] }}',
                                 y: [
-                                    new Date('{{ $dt['y'] }}').getTime(),
-                                    new Date('{{ $dt['z'] }}').getTime()
+                                    new Date('{{ $dt['created_at'] }}').getTime(),
+                                    new Date('{{ $dt['updated_at'] }}').getTime()
                                 ]
                             },
                         @endforeach
@@ -211,9 +205,9 @@
                     data: [
                         @foreach ($data as $dt)
                             {
-                                x: '{{ $dt['x'] }}',
-                                y: [new Date('{{ $dt['y'] }}').getTime(),
-                                    new Date('{{ $dt['z'] }}').getTime()
+                                x: '{{ $dt['id'] . ' ' . $dt['item_name'] }}',
+                                y: [new Date('{{ $dt['created_at'] }}').getTime(),
+                                    new Date('{{ $dt['updated_at'] }}').getTime()
                                 ]
                             },
                         @endforeach
@@ -221,19 +215,45 @@
                 }
             ],
             chart: {
-                height: 1000,
+                height: {{ $count }} * 60,
                 type: 'rangeBar',
                 offsetY: '15',
                 events: {
                     dataPointSelection: function(event, chartContext, config) {
-                        $('#asu').show();
+                        $('#asu').show(300);
+                        // $("#asu").css({
+                        //     "display": "block",
+                        //     "transition": "width 1s",
+                        // });
                         // console.log(config.w.globals.labels[config.dataPointIndex]);
                         // var x = console.log(config.w.globals.labels[config.dataPointIndex]);
                         // var y = console.log(config);
-                        console.log(data[config.dataPointIndex].y);
-                        document.getElementById("testing").innerHTML = data[config.dataPointIndex].x;
-                        // $('#testing').val('OK');
-                        // document.write(data[0]);
+                        console.log(data[config.dataPointIndex]);
+                        $('#reg_sp').text(data[config.dataPointIndex].reg_sp);
+                        $('#created_at').text(data[config.dataPointIndex].created_at);
+                        $('#updated_at').text(data[config.dataPointIndex].updated_at);
+                        $('#date').text(data[config.dataPointIndex].date);
+                        $('#nama_pic').text(data[config.dataPointIndex].nama_pic);
+                        $('#item_name').text(data[config.dataPointIndex].item_name);
+                        $('#date').text(data[config.dataPointIndex].date);
+                        $('#part_from').text(data[config.dataPointIndex].part_from);
+                        $('#section').text(data[config.dataPointIndex].section);
+                        $('#line').text(data[config.dataPointIndex].line);
+                        $('#machine').text(data[config.dataPointIndex].machine);
+                        $('#item_code').text(data[config.dataPointIndex].item_code);
+                        $('#item_type').text(data[config.dataPointIndex].item_type);
+                        $('#maker').text(data[config.dataPointIndex].maker);
+                        $('#problem').text(data[config.dataPointIndex].problem);
+                        $('#price').text(data[config.dataPointIndex].price);
+                        $('#place_of_repair').text(data[config.dataPointIndex].place_of_repair);
+                        $('#status_repair').text(data[config.dataPointIndex].status_repair);
+                        $('#progress').text(data[config.dataPointIndex].progress);
+                        $('#item_type').text(data[config.dataPointIndex].item_type);
+                        $('#analisa').text(data[config.dataPointIndex].analisa);
+                        $('#action').text(data[config.dataPointIndex].action);
+                        $('#plan_start_repair').text(data[config.dataPointIndex].plan_start_repair);
+                        $('#plan_finish_repair').text(data[config.dataPointIndex].plan_finish_repair);
+                        $('#item_type').text(data[config.dataPointIndex].item_type);
                     }
                 }
             },
@@ -294,237 +314,5 @@
 
         var chart = new ApexCharts(document.querySelector("#gantt"), options);
         chart.render();
-    </script>
-
-
-    <!-- script chart-qty-repair -->
-    <script>
-        var options = {
-            series: [{
-                name: 'Waiting',
-                type: 'column',
-                data: [
-                    @foreach ($qty['Waiting'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'On Progress',
-                type: 'column',
-                data: [
-                    @foreach ($qty['On Progress'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'Prep. Seal Kit',
-                type: 'column',
-                data: [
-                    @foreach ($qty['Seal Kit'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'Trial',
-                type: 'column',
-                data: [
-                    @foreach ($qty['Trial'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'Finish',
-                type: 'column',
-                data: [
-                    @foreach ($qty['Finish'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'Scrap',
-                type: 'column',
-                data: [
-                    @foreach ($qty['Scrap'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }, {
-                name: 'Input',
-                type: 'line',
-                data: [
-                    @foreach ($qty['total'] as $index => $key)
-                        {{ $key }},
-                    @endforeach
-                ]
-            }],
-            colors: [
-                '#fd7e14',
-                'rgba(0, 197, 220, 1)',
-                'rgba(52, 191, 163, 1)',
-                'rgba(115, 76, 234, 1)',
-                'rgba(255, 185, 0, 1)',
-                '#343a40',
-                'rgba(244, 81, 108, 1)',
-            ],
-            chart: {
-                type: 'line',
-                height: 350,
-                stacked: true,
-                toolbar: {
-                    show: true
-                },
-                zoom: {
-                    enabled: true
-                }
-            },
-            title: {
-                text: 'Qty Part Repair',
-                align: 'left'
-            },
-            dataLabels: {
-                enabled: true,
-                enabledOnSeries: [6]
-            },
-            responsive: [{
-                breakpoint: 480,
-                options: {
-                    legend: {
-                        position: 'bottom',
-                        offsetX: -10,
-                        offsetY: 0
-                    }
-                }
-            }],
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    borderRadius: 10,
-                    dataLabels: {
-                        total: {
-                            enabled: true,
-                            style: {
-                                fontSize: '13px',
-                                fontWeight: 900
-                            }
-                        }
-                    }
-                },
-            },
-            xaxis: {
-                categories: ['W1', 'W2', 'W3', 'W4',
-                    'W5',
-                ],
-            },
-            yaxis: {
-                title: {
-                    text: 'Qty'
-                },
-            },
-            legend: {
-                position: 'right',
-                offsetY: 40
-            },
-            fill: {
-                opacity: 1
-            }
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart-qty-repair"), options);
-        chart.render();
-    </script>
-
-    <!-- script chart-cost-saving -->
-    <script>
-        var options = {
-            series: [{
-                    name: "Cost Saving",
-                    data: [450, 520, 380, 240, 330, 260, 210, 200, 600, 800, 150, 100]
-                },
-                {
-                    name: 'Target',
-                    data: [1300, 1300, 1300, 1300, 1300, 1300, 1300, 1300, 1300, 1300, 1300, 1300]
-                }
-            ],
-            chart: {
-                height: 350,
-                type: 'line',
-                zoom: {
-                    enabled: false
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                width: [5, 7, 5],
-                curve: 'straight',
-                dashArray: [0, 8, 5]
-            },
-            title: {
-                text: 'Cost Saving',
-                align: 'left'
-            },
-            legend: {
-                tooltipHoverFormatter: function(val, opts) {
-                    return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
-                }
-            },
-            markers: {
-                size: 0,
-                hover: {
-                    sizeOffset: 6
-                }
-            },
-            xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-                    'Oct', 'Nov', 'Dec'
-                ],
-            },
-            yaxis: {
-                title: {
-                    text: 'Rupiah (in Million)'
-                },
-            },
-            tooltip: {
-                y: [{
-                        title: {
-                            formatter: function(val) {
-                                return val + " (mins)"
-                            }
-                        }
-                    },
-                    {
-                        title: {
-                            formatter: function(val) {
-                                return val + " per session"
-                            }
-                        }
-                    },
-                    {
-                        title: {
-                            formatter: function(val) {
-                                return val;
-                            }
-                        }
-                    }
-                ]
-            },
-            grid: {
-                borderColor: '#f1f1f1',
-            }
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart-cost-saving"), options);
-        chart.render();
-    </script>
-
-    <script>
-        $(document).ready(function() {
-            $('#filter_month').on('input', function() {
-                var month = $('#filter_month').val()
-
-                alert(month)
-            });
-        });
     </script>
 @endsection
