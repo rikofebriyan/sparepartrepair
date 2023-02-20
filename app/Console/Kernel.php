82f9482a02ff876765b74079976c0feb56baf53c
rikofebriyan <rikofebriyan@gmail.com>
2023-02-20 08:21:56 +0700

    nonaktikamn function email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,53 +29,49 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
-        $schedule->call(function () {
-            // $progressrepair = Progressrepair::where('plan_finish_repair', '<', Carbon::now())
-            // ->whereNull('notified_at')->get();
+        // $schedule->call(function () {
 
-            $progressrepair = DB::table('waitingrepairs')
-            ->join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
-            ->select('waitingrepairs.*', 
-            'progressrepairs.place_of_repair', 
-            'progressrepairs.analisa', 'progressrepairs.action', 
-            'progressrepairs.plan_start_repair', 
-            'progressrepairs.plan_finish_repair', 
-            'progressrepairs.pic_repair', 
-            'progressrepairs.actual_finish_repair', 
-            'progressrepairs.plan_start_revision', 
-            'progressrepairs.plan_finish_revision', 
-            'progressrepairs.reason_revision', 
-            'progressrepairs.id as progressid', 
-            'progressrepairs.notified_at')
-            ->where('progress', '<>', 'finish')
-            ->where('progress', '<>', 'Scrap')
-            ->where('plan_finish_repair', '<', Carbon::now())
-            ->whereNull('notified_at')->get();
+        //     $progressrepair = DB::table('waitingrepairs')
+        //     ->join('progressrepairs', 'waitingrepairs.id', '=', 'progressrepairs.form_input_id')
+        //     ->select('waitingrepairs.*', 
+        //     'progressrepairs.place_of_repair', 
+        //     'progressrepairs.analisa', 'progressrepairs.action', 
+        //     'progressrepairs.plan_start_repair', 
+        //     'progressrepairs.plan_finish_repair', 
+        //     'progressrepairs.pic_repair', 
+        //     'progressrepairs.actual_finish_repair', 
+        //     'progressrepairs.plan_start_revision', 
+        //     'progressrepairs.plan_finish_revision', 
+        //     'progressrepairs.reason_revision', 
+        //     'progressrepairs.id as progressid', 
+        //     'progressrepairs.notified_at')
+        //     ->where('progress', '<>', 'finish')
+        //     ->where('progress', '<>', 'Scrap')
+        //     ->where('plan_finish_repair', '<', Carbon::now())
+        //     ->whereNull('notified_at')->get();
 
 
         
-            foreach ($progressrepair as $repair) {
-                $data = [
-                    'name' => $repair->pic_repair,
-                    'item_name' => $repair->item_name,
-                    'item_code' => $repair->item_code,
-                    'plan_finish_repair' => Carbon::parse($repair->plan_finish_repair)->format('d-M-Y'),
-                    'reason' => 'Repair is delayed',
-                ];
+        //     foreach ($progressrepair as $repair) {
+        //         $data = [
+        //             'name' => $repair->pic_repair,
+        //             'item_name' => $repair->item_name,
+        //             'item_code' => $repair->item_code,
+        //             'plan_finish_repair' => Carbon::parse($repair->plan_finish_repair)->format('d-M-Y'),
+        //             'reason' => 'Repair is delayed',
+        //         ];
         
-                Mail::send('emails.demo', $data, function ($message) use ($repair) {
-                    $message->to('rikofebriyan@gmail.com', 'PE-Digitalization')
-                    ->subject('I-Mirs Delay Notification for Your Repair');
-                $message->from('febriyanomov@gmail.com', 'PE-Digitalization');
-                });
+        //         Mail::send('emails.demo', $data, function ($message) use ($repair) {
+        //             $message->to('rikofebriyan@gmail.com', 'PE-Digitalization')
+        //             ->subject('I-Mirs Delay Notification for Your Repair');
+        //         $message->from('febriyanomov@gmail.com', 'PE-Digitalization');
+        //         });
                 
-                $notified = Progressrepair::find($repair->progressid);
-                $notified->notified_at = Carbon::now();
-                $notified->save();
-            }
-        })->everyMinute();
+        //         $notified = Progressrepair::find($repair->progressid);
+        //         $notified->notified_at = Carbon::now();
+        //         $notified->save();
+        //     }
+        // })->everyHour();
         
         
 
